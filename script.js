fecb604a5a4a9e5bd5920a5cf5825981ce2e0976
MMXDou <mmxdou@gmail.com>
2023-03-09 20:49:19 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-// const cards = document.querySelectorAll('.card');
+const cards = document.querySelectorAll('.card');
 
-// cards.forEach((card) => {
-//     card.addEventListener('click', () => {
-//         if (!(card.classList.contains("expanded"))) {
-//             cards.forEach((cardEach) => {
-//                 cardEach.classList.remove('expanded');
-//             })
-//             card.classList.add('expanded');
-//         }
-//     })
-// })
-
-console.log("hello world");
+cards.forEach((card) => {
+    card.addEventListener('click', () => {
+        if (!(card.classList.contains("expanded"))) {
+            cards.forEach((cardEach) => {
+                cardEach.classList.remove('expanded');
+            })
+            card.classList.add('expanded');
+        }
+    })
+})
