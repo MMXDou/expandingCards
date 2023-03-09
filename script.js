498badc2ecef13d2f61359a8d96b768cfc51517a
MMXDou <mmxdou@gmail.com>
2023-03-09 19:43:10 +1100

    last version the div click not working
    try change div to button

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-const cards = document.querySelectorAll('.card');
-// console.log(cards[0]);
+var cards = document.querySelectorAll('.card');
 
-cards[0].addEventListener('click', function () {
-    console.log(this);
-})
-
-for (i = 0; i < cards.length; i++) {
+for (var i = 0; i < cards.length; i++) {
     cards[i].addEventListener('click', function () {
-        console.log(this.classList.contains("expanded"));
+
+
         if (!(this.classList.contains("expanded"))) {
             // cards.classList.remove('expanded');
             cards.forEach((card) => {
